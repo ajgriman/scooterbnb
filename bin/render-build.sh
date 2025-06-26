9ddaac725b9e83007dfb83fac5cb690022eb069b
Antonio Griman <griman@gmail.com>
2025-06-26 05:17:10 -0400

    render-build.sh script 1.1

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,7 +6,8 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
-# This single command handles both initial setup and all future migrations.
-# The environment variable is only needed for the very first deploy.
+echo "--- Waiting 15 seconds for database to be ready... ---"
+sleep 15
+
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:queue
